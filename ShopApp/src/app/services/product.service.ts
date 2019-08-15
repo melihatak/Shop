a799b9178acef94877d76eeeb17c0b11d3c12fd5
melihatak <melihatak2015@gmail.com>
2019-08-15 21:32:23 +0300

    using observable,  to catcherror and log data via pipe

--- ShopApp/src/app/services/product.service.ts
+++ ShopApp/src/app/services/product.service.ts
@@ -1,14 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Product } from '../product/Product';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators'
 @Injectable()
 export class ProductService {
 
   constructor(private http: HttpClient) { }
-  path="http://localhost:3000/products"
+  path = "http://localhost:3000/products"
 
-  getProducts()
-  {
-  return  this.http.get<Product[]>(this.path);
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.path).pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    );
   }
+  handleError(err: HttpErrorResponse) {
+    let errormessage = "";
+    if (err.error instanceof ErrorEvent) {
+      errormessage = "Bir hata oluştu" + err.error.message
+    }
+    else {
+      errormessage = "Sistemsel bir hata "
+
+    }
+    return throwError(errormessage)
+
+  }
+
+
+
+
 }
